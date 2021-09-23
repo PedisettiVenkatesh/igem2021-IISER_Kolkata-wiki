e1f85d8103c26a4d18caff1f101d8eb5a54bff6f
PedisettiVenkatesh <PedisettiVenkatesh@gmail.com>
2021-09-23 13:53:30 +0530

    2DG

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const progressBar = document.getElementById("progressBar");
 const scrollPath = document.getElementById("scrollPath");
 const topBtn = document.getElementById("toTop");
 const overlayAppearValue = 300;
+const preloader = document.getElementById("preloader");
+
+function preload() {
+  preloader.style.opacity = "0";
+  preloader.style.zIndex = "-999999";
+  document.documentElement.scrollTop = 0;
+  document.body.classList.remove("noScroll");
+}
 
 window.onscroll = function () {
   let totalHeight = document.body.scrollHeight - window.innerHeight;
@@ -11,9 +19,11 @@ window.onscroll = function () {
   console.log(window.pageYOffset, document.body.scrollTop, document.documentElement.scrollTop);
   if (document.body.scrollTop > overlayAppearValue || document.documentElement.scrollTop > overlayAppearValue || window.pageYOffset > overlayAppearValue) {
     topBtn.style.height = "50px";
+    topBtn.style.opacity = "1";
     scrollPath.style.opacity = "1";
   } else {
     topBtn.style.height = "0";
+    topBtn.style.opacity = "0";
     scrollPath.style.opacity = "0";
   }
 };
